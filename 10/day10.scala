ad5674f60741ca9ae37fe5c6ebae1e2da49f397e
Matthew Zimmerman <matt.zimmerman@ccri.com>
2017-12-11 13:22:50 -0500

    Second half of day 10.

--- 10/day10.scala
+++ 10/day10.scala
@@ -8,31 +8,84 @@ implicit class RichSeq[T](s: Seq[T]) {
   }
 }
 
-def process(s: Seq[Int], start: Int, length: Int): Seq[Int] = {
+def knot(s: Seq[Int], start: Int, length: Int): Seq[Int] = {
   val (a, b) = s.rotate(start).splitAt(length)
   (a.reverse ++ b).rotate(-start)
 }
 
-def processAll(s: Seq[Int], lengths: List[Int]): Seq[Int] = processAll(s, 0, 0, lengths)
+case class RoundState(seq: Seq[Int], start: Int, skip: Int)
+
+def calcRound(s: Seq[Int], lengths: List[Int], debug: Boolean = false): RoundState = calcRound(RoundState(s, 0, 0), lengths, debug)
 
 @tailrec
-def processAll(s: Seq[Int], start: Int, skip: Int, lengths: List[Int]): Seq[Int] = {
-  println(s)
+def calcRound(r: RoundState, lengths: List[Int], debug: Boolean): RoundState = {
+  if (debug)
+    println(r)
   if (lengths.isEmpty)
-    s
+    r
   else {
-    val sNew = process(s, start, lengths.head)
-    processAll(sNew, (start + lengths.head + skip) % s.length, skip + 1, lengths.tail)
+    val rNew = RoundState(
+      knot(r.seq, r.start, lengths.head),
+      (r.start + lengths.head + r.skip) % r.seq.length,
+      r.skip + 1)
+    calcRound(rNew, lengths.tail, debug)
   }
 }
 
+def part1(lengths: List[Int]): Int = {
+  val input = (0 to 255).toVector
+  val out = calcRound(input, lengths)
+  out.seq(0) * out.seq(1)
+}
+
+@tailrec
+def calcSparseHash(round: RoundState, lengths: List[Int], n: Int): RoundState = {
+  if (n == 64)
+    round
+  else {
+    val roundNew = calcRound(round, lengths, false)
+    calcSparseHash(roundNew, lengths, n+1)
+  }
+}
+
+def calcDenseHash(in: Seq[Int]): String = {
+  in.sliding(16, 16).map(_.reduce(_ ^ _).formatted("%02x")).mkString("")
+}
+
+def hash(s: String): String = {
+  val seed = List(17, 31, 73, 47, 23)
+  val sList = s.toList.map(_.toInt) ++ seed
+  //println(s"sList=$sList")
+  val initVector = (0 to 255).toVector
+  val sparse = calcSparseHash(RoundState(initVector, 0, 0), sList, 0)
+  calcDenseHash(sparse.seq)
+}
+
+println("--- Test #1 ---")
 val test = Vector(0, 1, 2, 3, 4)
 val testLengths = List(3, 4, 1, 5)
-processAll(test, testLengths)
+calcRound(test, testLengths, true)
 
-val input = (0 to 255).toVector
-val inputLengths = Source.fromFile("input.txt").getLines().mkString("").split(",").map(_.toInt).toList
-println(input)
+println()
+println("--- Part #1 ---")
+//val input = (0 to 255).toVector
+val input = Source.fromFile("input.txt").getLines().mkString("")
+val inputLengths = input.split(",").map(_.toInt).toList
+//println(input)
 println(inputLengths)
-val out = processAll(input, inputLengths)
-println(out(0) * out(1))
+val out = part1(inputLengths)
+println(out)
+
+println()
+println("--- Test #2 ---")
+val testDense = Vector(65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22)
+println(s"$testDense => ${calcDenseHash(testDense)}")
+println()
+val test2 = List("", "AoC 2017", "1,2,3", "1,2,4")
+for (t <- test2) {
+  println(s"${hash(t)} <= $t")
+}
+
+println()
+println("--- Part #2 ---")
+println(hash(input))
